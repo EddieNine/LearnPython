1f499871a6a218da41f28ec84b1c927a46a72921
EddieNine <believefixed@hotmail.com>
2025-01-04 10:02:40 -0300

    Dobro, Triplo, Raiz Quadrada

---
+++ Exercicios/ex006.py
@@ -0,0 +1,10 @@
+import math
+
+numero = float(input("Digite um número: "))
+d = numero * 2
+t = numero * 3
+r = math.sqrt(numero)
+
+print(f"O dobro de {numero} vale {d}.")
+print(f"O triplo de {numero} vale {t}.")
+print(f"A raiz quadrada de {numero} é igual a {r:.2f}.")
