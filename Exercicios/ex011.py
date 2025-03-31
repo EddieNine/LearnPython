a49f11479e1f74445326bdd808711c3cf7e08060
EddieNine <believefixed@hotmail.com>
2025-03-31 17:13:25 -0300

    exercicio 11

---
+++ Exercicios/ex011.py
@@ -0,0 +1,8 @@
+l = float(input("Largura da parede: "))
+al = float(input("Altura da parede: "))
+
+a = l * al
+litros = a / 2
+
+print(f"Sua parede tem a dimensão de {l}x{al} e sua área é de {a}m²")
+print(f"Para pintar essa parede, você precisará de {litros}l de tinta.")
